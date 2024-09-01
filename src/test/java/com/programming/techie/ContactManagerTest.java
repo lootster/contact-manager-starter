8aad5500a37bffc4c4ebccb84b285e58453a2aa3
Sheldon <sheldonkhlim@gmail.com>
2024-09-01 22:24:21 +0800

    Add test cases for Runtime Exception

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,6 +1,7 @@
 package com.programming.techie;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ContactManagerTest {
@@ -9,8 +10,44 @@ class ContactManagerTest {
     public void shouldCreateContact() {
         ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "0123456789");
+
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        Assertions.assertTrue(contactManager.getAllContacts().stream()
+                .anyMatch(contact -> contact.getFirstName().equals("John") &&
+                        contact.getLastName().equals("Doe") &&
+                        contact.getPhoneNumber().equals("0123456789")));
     }
+
+    @Test
+    @DisplayName("Should Not Create Contact When First Name Is Null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Doe", "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Last Name Is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", null, "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Phone Number Is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", "Doe", null);
+        });
+    }
+
 
 }
